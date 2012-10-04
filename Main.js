bb90a3a11184537975fa42f95120e649578d42ef
Daniel Reyes <Kurlzs@Daniels-MacBook-Pro.local>
2012-10-04 17:54:44 -0400

    Added clear survey, get data and save data to js file

--- Main.js
+++ Main.js
@@ -1,7 +1,35 @@
-/*
-Daniel Reyes
-VFW-1210
-Project 2
-Oct 4th
-*/ 
+// Daniel Reyes
+// VFW-1210
+// Project 2
+// Oct 14
+
+// DOM Readiness
+window.addEventListener("DOMContentLoaded", function(){
+
+	//fuction getElementById
+	function $(x){
+		var theElement = document.getElementById(x);
+		return theElement;		
+	}
+	
+	//variable default
+	var establishmentGroups ["--Choose an Establishment", "Chili's", "Applebee's", "Hooters", "Wal-Mart", "Target"];
+	
+	
+	
+	//Set link & Submit click events
+	var displayData = $('display data');
+	displayData.addEventLister("click", getData);
+	var clearSurvey = $('clear');
+	clearSurvey.addEventLister("click", clearSurvey);
+	var submitSurvey = $('submit');
+	submitSurvey.addEventLister("click", storeData);
+	
+	
+	
+	
+	
+	
+	
+});
 
